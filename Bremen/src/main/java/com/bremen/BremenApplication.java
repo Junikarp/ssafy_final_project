d9af918a6b9cbb14fdf6020f9f27ab5e124afb8d
오건영 <o54711254@ogeon-yeong-ui-MacBookAir.local>
2023-11-14 13:48:39 +0900

    feat  : 프로젝트 생성

---
+++ Bremen/src/main/java/com/bremen/BremenApplication.java
@@ -0,0 +1,13 @@
+package com.bremen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BremenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BremenApplication.class, args);
+	}
+
+}
