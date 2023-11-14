b31c33008bd73d9b8aa0b6a39b24ca3a224dca0b
Junikarp <zieunpk0924@gmail.com>
2023-11-14 17:09:05 +0900

    feat : 로고 생성

--- bremen_vue/src/App.vue
+++ bremen_vue/src/App.vue
@@ -1,11 +1,13 @@
-<script setup>
-</script>
-
 <template>
   <header>
+  <NavHeader></NavHeader>
   </header>
-
 </template>
+
+<script setup>
+import NavHeader from './components/NavHeader.vue';
+
+</script>
 
 <style scoped>
 
---
+++ bremen_vue/src/components/NavHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    
+<div class="main-container">
+<a class="main-header" href="https://www.naver.com">
+    <img src="../assets/logo.png" id="logo">
+    𝕭𝖗𝖊𝖒𝖊𝖓
+</a>
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main-container {
+    margin-left: 200px;
+    margin-right: 200px;
+}
+.main-header {
+    background-color: white;
+    font-size: 40px;
+    text-decoration: none;
+    color:black;
+}
+#logo {
+    width:30px;
+    
+}
+</style>
