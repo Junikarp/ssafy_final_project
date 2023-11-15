e5ac7101726210fccae7d48a22e08446c7d51136
Junikarp <zieunpk0924@gmail.com>
2023-11-15 14:20:13 +0900

    feat : 로그인 컴포넌트 추가

--- bremen_vue/src/components/NavHeader.vue
+++ bremen_vue/src/components/NavHeader.vue
@@ -6,7 +6,7 @@
         </RouterLink>
         <div class="login-bar">
             <RouterLink :to="{name:'community'}">커뮤니티</RouterLink>
-            <a href="../views/Community.vue">Login</a>
+            <RouterLink :to="{name:'login'}">Login</RouterLink>
             <a href="Sign in">Sign up</a>
         </div>
     </div>
--- bremen_vue/src/views/HomeView.vue
+++ bremen_vue/src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="main">
-    
+    <MainPage1></MainPage1>
+    <MainPage2></MainPage2>
+    <MainPage3></MainPage3>
   </div>
 </template>
 
 <script setup>
-
+import MainPage1 from '../components/MainPage/MainPage1.vue';
+import MainPage2 from '../components/MainPage/MainPage2.vue';
+import MainPage3 from '../components/MainPage/MainPage3.vue';
 </script>
 
 <style scoped>
 .main {
-    margin-left: 200px;
-    margin-right: 200px;
+  /* margin-left: 200px;
+    margin-right: 200px; */
 }
 </style>
