cd6f8a7357cb1e4ce049f4e452bd0a78b8e152c6
Junikarp <zieunpk0924@gmail.com>
2023-11-17 10:25:23 +0900

    feat : service 클래스 transactional 추가

--- Bremen/src/main/java/com/bremen/model/service/BoardServiceImpl.java
+++ Bremen/src/main/java/com/bremen/model/service/BoardServiceImpl.java
@@ -3,10 +3,13 @@ package com.bremen.model.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bremen.model.dao.BoardDao;
 import com.bremen.model.dto.Board;
 
+@Service
 public class BoardServiceImpl implements BoardService {
 	
 	@Autowired
@@ -22,21 +25,25 @@ public class BoardServiceImpl implements BoardService {
 		return dao.selectOne(id);
 	}
 
+	@Transactional
 	@Override
 	public int insertBoard(Board board) {
 		return dao.insertBoard(board);
 	}
 
+	@Transactional
 	@Override
 	public void deleteBoard(int id) {
 		dao.deleteBoard(id);
 	}
 
+	@Transactional
 	@Override
 	public void updateBoard(Board board) {
 		dao.updateBoard(board);
 	}
 
+	@Transactional
 	@Override
 	public void updateViewCnt(int id) {
 		dao.updateViewCnt(id);
--- Bremen/src/main/java/com/bremen/model/service/ReviewServiceImpl.java
+++ Bremen/src/main/java/com/bremen/model/service/ReviewServiceImpl.java
@@ -3,10 +3,13 @@ package com.bremen.model.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bremen.model.dao.ReviewDao;
 import com.bremen.model.dto.Review;
 
+@Service
 public class ReviewServiceImpl implements ReviewService {
 
 	@Autowired
@@ -17,16 +20,19 @@ public class ReviewServiceImpl implements ReviewService {
 		return dao.selectAll();
 	}
 
+	@Transactional
 	@Override
 	public int insertReview(Review review) {
 		return dao.inservReview(review);
 	}
 
+	@Transactional
 	@Override
 	public void deleteReview(int id) {
 		dao.deleteReview(id);
 	}
 
+	@Transactional
 	@Override
 	public void updateReview(Review review) {
 		dao.updateReview(review);;
