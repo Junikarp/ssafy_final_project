286e6afc75dff13a976686eef4196938fcedf453
Junikarp <zieunpk0924@gmail.com>
2023-11-17 10:23:47 +0900

    feat : board, review service 구현

--- Bremen/src/main/java/com/bremen/model/service/BoardServiceImpl.java
+++ Bremen/src/main/java/com/bremen/model/service/BoardServiceImpl.java
@@ -1,5 +1,46 @@
 package com.bremen.model.service;
 
-public class BoardServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bremen.model.dao.BoardDao;
+import com.bremen.model.dto.Board;
+
+public class BoardServiceImpl implements BoardService {
+	
+	@Autowired
+	BoardDao dao;
+
+	@Override
+	public List<Board> selectAll() {
+		return dao.selectAll();
+	}
+
+	@Override
+	public Board selectOne(int id) {
+		return dao.selectOne(id);
+	}
+
+	@Override
+	public int insertBoard(Board board) {
+		return dao.insertBoard(board);
+	}
+
+	@Override
+	public void deleteBoard(int id) {
+		dao.deleteBoard(id);
+	}
+
+	@Override
+	public void updateBoard(Board board) {
+		dao.updateBoard(board);
+	}
+
+	@Override
+	public void updateViewCnt(int id) {
+		dao.updateViewCnt(id);
+	}
+	
+	
 }
--- Bremen/src/main/java/com/bremen/model/service/ReviewServiceImpl.java
+++ Bremen/src/main/java/com/bremen/model/service/ReviewServiceImpl.java
@@ -1,5 +1,35 @@
 package com.bremen.model.service;
 
-public class ReviewServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bremen.model.dao.ReviewDao;
+import com.bremen.model.dto.Review;
+
+public class ReviewServiceImpl implements ReviewService {
+
+	@Autowired
+	ReviewDao dao;
+	
+	@Override
+	public List<Review> selectAll() {
+		return dao.selectAll();
+	}
+
+	@Override
+	public int insertReview(Review review) {
+		return dao.inservReview(review);
+	}
+
+	@Override
+	public void deleteReview(int id) {
+		dao.deleteReview(id);
+	}
+
+	@Override
+	public void updateReview(Review review) {
+		dao.updateReview(review);;
+	}
 
 }
