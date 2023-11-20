2f464a8877cb1b83b48112360dd11f834f42a1d3
오건영 <o54711254@ogeon-yeong-ui-MacBookAir.local>
2023-11-20 22:09:56 +0900

    style : category 스타일 변경

--- bremen_vue/src/components/Community/CategoryView.vue
+++ bremen_vue/src/components/Community/CategoryView.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <div class="category-image"></div>
         <div class="logo"><router-link :to="{ name: 'home' }"
                 style="text-decoration: none; color: black;">BREMEN</router-link>
         </div>
@@ -18,7 +19,19 @@
                         class="button-img"><span class="button-text">사이클</span></button></RouterLink>
             <RouterLink to="/board/badminton"><button class="button-color"><img src="../../assets/category/badminton.png"
                         class="button-img"><span class="button-text">배드민턴</span></button></RouterLink>
-
+            <RouterLink to="/board/soccer"><button class="button-color"><img src="../../assets/category/soccer.png"
+                        class="button-img">
+                    <span class="button-text">축구</span></button></RouterLink>
+            <RouterLink to="/board/volleyball"><button class="button-color"><img src="../../assets/category/volleyball.png"
+                        class="button-img"><span class="button-text">배구</span></button></RouterLink>
+            <RouterLink to="/board/running"><button class="button-color"><img src="../../assets/category/running.png"
+                        class="button-img"><span class="button-text">런닝</span></button></RouterLink>
+            <RouterLink to="/board/swimming"><button class="button-color"><img src="../../assets/category/swimming.png"
+                        class="button-img"><span class="button-text">수영</span></button></RouterLink>
+            <RouterLink to="/board/cycle"><button class="button-color"><img src="../../assets/category/cycle.png"
+                        class="button-img"><span class="button-text">사이클</span></button></RouterLink>
+            <RouterLink to="/board/badminton"><button class="button-color"><img src="../../assets/category/badminton.png"
+                        class="button-img"><span class="button-text">배드민턴</span></button></RouterLink>
         </div>
     </div>
 </template>
@@ -28,6 +41,17 @@
 </script>
 
 <style scoped>
+
+.category-image {
+    background-image: url("../../assets/main/community.jpeg");
+    height: 350px;
+    width: 100%;
+    background-size: cover;
+    background-position: center;
+    margin-bottom: 30px;
+    
+    
+}
 .logo {
     font-weight: 800;
     font-size: 40px;
@@ -48,7 +72,7 @@
     background-color: rgb(255, 255, 255);
     border-radius: 30%;
     border-color: rgb(185, 192, 198);
-    margin: 20px;
+    margin: 30px;
 }
 
 .button-img {
